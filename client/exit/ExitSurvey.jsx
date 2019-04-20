d74d61b4bf8b48a09d29a2d5a406628f04f5eb05
askyel <ariel.s.levy1@gmail.com>
2019-04-20 17:35:29 -0400

    exit survey demographic and styling

--- client/exit/ExitSurvey.jsx
+++ client/exit/ExitSurvey.jsx
@@ -27,6 +27,10 @@ import {
 export default class ExitSurvey extends React.Component {
   static stepName = "ExitSurvey";
   state = {
+    age: "",
+    gender: "",
+    country: "",
+    education: "",
     visits: "",
     fair: "",
     original: "",
@@ -47,7 +51,7 @@ export default class ExitSurvey extends React.Component {
 
   render() {
     const { player } = this.props;
-    const { visits, fair, original, home, creative, jobTitle } = this.state;
+    const { age, gender, country, education, visits, fair, original, home, creative, jobTitle } = this.state;
 
     return (
       <Centered>
@@ -59,6 +63,94 @@ export default class ExitSurvey extends React.Component {
             yourself:
           </p>
           <form onSubmit={this.handleSubmit}>
+            <div className="form-line thirds">
+              <div>
+                <label className="bp3-label" htmlFor="age">Age</label>
+                <div>
+                  <input
+                    id="age"
+                    type="number"
+                    min="0"
+                    max="150"
+                    step="1"
+                    dir="auto"
+                    name="age"
+                    value={age}
+                    onChange={this.handleChange}
+                  />
+                </div>
+              </div>
+              <div>
+                <label className="bp3-label" htmlFor="gender">Gender</label>
+                <div>
+                  <input
+                    id="gender"
+                    type="text"
+                    dir="auto"
+                    name="gender"
+                    value={gender}
+                    onChange={this.handleChange}
+                    autoComplete="off"
+                  />
+                </div>
+              </div>
+              <div>
+                <label className="bp3-label" htmlFor="country">Country</label>
+                <div>
+                  <input
+                    id="country"
+                    type="text"
+                    dir="auto"
+                    name="country"
+                    value={country}
+                    onChange={this.handleChange}
+                    autoComplete="off"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="form-line">
+              <div>
+                <RadioGroup
+                  inline={false}
+                  label="Highest Education Qualification"
+                  name="education"
+                  onChange={this.handleChange}
+                  selectedValue={education}
+                >
+                  <Radio
+                    selected={education}
+                    name="education"
+                    value="high-school"
+                    label="High School"
+                    onChange={this.handleChange}
+                  />
+                  <Radio
+                    selected={education}
+                    name="education"
+                    value="bachelor"
+                    label="US Bachelor's Degree"
+                    onChange={this.handleChange}
+                  />
+                  <Radio
+                    selected={education}
+                    name="education"
+                    value="master"
+                    label="Master's or higher"
+                    onChange={this.handleChange}
+                  />
+                  <Radio
+                    selected={education}
+                    name="education"
+                    value="other"
+                    label="Other"
+                    onChange={this.handleChange}
+                  />
+                </RadioGroup>
+              </div>
+            </div>
+
             <div className="form-line">
               <RadioGroup
                 inline={false}
